d12a7898d37dfb98439f12d12c416010abe858c3
Andrew Clarke <andreweskeclarke@gmail.com>
2019-03-29 11:34:52 +0000

    Allow running experiments against specified style/content images

--- transfer.py
+++ transfer.py
@@ -15,37 +15,37 @@ from shared_utils import load_img, save_img, merge_images
 from torch.autograd import Variable
 from torchvision import datasets, models, transforms, utils
 
-CONTENT_IMAGE_PATH = './images/content.jpg'
-STYLE_IMAGE_PATH = './images/style.jpg'
-STYLE_IMAGE = load_img(STYLE_IMAGE_PATH).cuda()
-CONTENT_IMAGE = load_img(CONTENT_IMAGE_PATH).cuda()
 
-def run_style_transfer(content_layers, style_layers):
+def run_style_transfer(content_layers, style_layers, base_dir='images'):
+    content_image_path = os.path.join(base_dir, 'content.jpg')
+    style_image_path = os.path.join(base_dir, 'style.jpg')
+    style_image = load_img(style_image_path).cuda()
+    content_image = load_img(content_image_path).cuda()
     if not isinstance(content_layers, (list,)):
         content_layers = [content_layers]
     if not isinstance(style_layers, (list,)):
         style_layers = [style_layers]
     id = '.'.join(str(x) for x in style_layers) + '___' + '.'.join(str(x) for x in content_layers)
-    image_dir = os.path.join('./images/', id)
+    image_dir = os.path.join(base_dir, id)
     def path_for(p):
         return os.path.join(image_dir, p)
 
     os.makedirs(image_dir, exist_ok=True)
-    save_img(CONTENT_IMAGE, path_for('transfer_step_00000.png'))
-    save_img(CONTENT_IMAGE, path_for('content.png'))
-    save_img(STYLE_IMAGE, path_for('style.png'))
-    stylized_image = Variable(CONTENT_IMAGE.data.clone(), requires_grad=True).cuda()
+    save_img(content_image, path_for('transfer_step_00000.png'))
+    save_img(content_image, path_for('content.png'))
+    save_img(style_image, path_for('style.png'))
+    stylized_image = Variable(content_image.data.clone(), requires_grad=True).cuda()
     loss_layers = content_layers + style_layers
     vgg = Vgg19(loss_layers).cuda()
 
     content_targets = list()
-    for t in vgg(CONTENT_IMAGE, content_layers):
+    for t in vgg(content_image, content_layers):
         t.detach()
         content_targets.append(t)
     content_loss_fns = [nn.MSELoss().cuda()] * len(content_targets)
 
     style_targets = list()
-    for t in vgg(STYLE_IMAGE, style_layers):
+    for t in vgg(style_image, style_layers):
         t.detach()
         style_targets.append(GramMatrix()(t))
     style_loss_fns = [StyleLoss().cuda()] * len(style_targets)
@@ -64,8 +64,9 @@ def run_style_transfer(content_layers, style_layers):
             total_loss.backward()
             return total_loss
         optimizer.step(single_step)
-        print('Iteration: {}, id: {}'.format(i, id))
-        save_img(stylized_image, path_for('transfer_step_{}.png'.format(str(i).zfill(5))))
+        if i % 10 == 0:
+            print('Iteration: {}, id: {}'.format(i, id))
+            save_img(stylized_image, path_for('transfer_step_{}.png'.format(str(i).zfill(5))))
     save_img(stylized_image, path_for('transfer.png'))
     merge_images(
             [path_for('content.png'), path_for('style.png'), path_for('transfer.png')],
@@ -73,12 +74,15 @@ def run_style_transfer(content_layers, style_layers):
 
 
 if __name__ == "__main__":
-    for i in range(2,17):
-        run_style_transfer(Vgg19.ALL_LAYERS[i:], Vgg19.ALL_LAYERS[:i])
-        run_style_transfer(Vgg19.ALL_LAYERS[i], Vgg19.ALL_LAYERS[:i])
-        run_style_transfer(Vgg19.ALL_LAYERS[i:i+2], Vgg19.ALL_LAYERS[:i])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', '-d', type=str, default='images') 
+    args = parser.parse_args()
+    for i in range(2,16):
+        run_style_transfer(Vgg19.ALL_LAYERS[i:], Vgg19.ALL_LAYERS[:i], args.directory)
+        run_style_transfer(Vgg19.ALL_LAYERS[i], Vgg19.ALL_LAYERS[:i], args.directory)
+        run_style_transfer(Vgg19.ALL_LAYERS[i:i+2], Vgg19.ALL_LAYERS[:i], args.directory)
 
-        run_style_transfer(Vgg19.ALL_LAYERS[i:], Vgg19.ALL_LAYERS)
-        run_style_transfer(Vgg19.ALL_LAYERS[i], Vgg19.ALL_LAYERS)
-        run_style_transfer(Vgg19.ALL_LAYERS[i:i+2], Vgg19.ALL_LAYERS)
+        run_style_transfer(Vgg19.ALL_LAYERS[i:], Vgg19.ALL_LAYERS, args.directory)
+        run_style_transfer(Vgg19.ALL_LAYERS[i], Vgg19.ALL_LAYERS, args.directory)
+        run_style_transfer(Vgg19.ALL_LAYERS[i:i+2], Vgg19.ALL_LAYERS, args.directory)
 
